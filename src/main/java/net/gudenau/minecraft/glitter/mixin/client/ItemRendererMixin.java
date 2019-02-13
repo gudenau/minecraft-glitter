689c8b7938b53a1acbf199ed14b141b0b8e5a122
gudenau <gudenaua@gmail.com>
2019-02-13 15:48:37 -0500

    Made the mixin not overwrite stuff

--- src/main/java/net/gudenau/minecraft/glitter/mixin/client/ItemRendererMixin.java
+++ src/main/java/net/gudenau/minecraft/glitter/mixin/client/ItemRendererMixin.java
@@ -1,66 +1,44 @@
 package net.gudenau.minecraft.glitter.mixin.client;
 
-import com.mojang.blaze3d.platform.GlStateManager;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.render.item.ItemDynamicRenderer;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
-import net.minecraft.client.texture.TextureManager;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resource.ResourceReloadListener;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin implements ResourceReloadListener {
-    @Shadow @Final
-    private TextureManager textureManager;
-
-    @Shadow
-    private void renderItemModel(BakedModel bakedModel_1, ItemStack itemStack_1){}
-
-    @Shadow
-    public static void renderGlint(TextureManager textureManager_1, Runnable runnable_1, int int_1){}
-
-    @Shadow
-    private void renderModelWithTint(BakedModel bakedModel_1, int int_1){}
-
-    /**
-     * @author gudenau
-     * @reason Could not just hook the lambda.
-     */
-    @Overwrite
-    public void renderItemAndGlow(ItemStack stack, BakedModel model) {
-        if (!stack.isEmpty()) {
-            GlStateManager.pushMatrix();
-            GlStateManager.translatef(-0.5F, -0.5F, -0.5F);
-            if (model.isBuiltin()) {
-                GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-                GlStateManager.enableRescaleNormal();
-                ItemDynamicRenderer.INSTANCE.render(stack);
-            } else {
-                this.renderItemModel(model, stack);
-                if (stack.hasEnchantmentGlint()) {
-                    renderGlint(this.textureManager, () -> {
-                        int color = 0xFF8040CC;
-                        CompoundTag tag = stack.getTag();
-                        if(tag != null && tag.containsKey("gud")){
-                            CompoundTag gudTag = tag.getCompound("gud");
-                            if(gudTag.containsKey("glitter")){
-                                color = gudTag.getInt("glitter") | 0xFF000000;
-                            }
-                        }
-                        this.renderModelWithTint(model, color);
-                    }, 8);
+    private final ThreadLocal<ItemStack> renderStack = new ThreadLocal<>();
+    
+    @Inject(method = "renderItemAndGlow", at = @At("HEAD"))
+    public void renderItemAndGlow(ItemStack stack, BakedModel model, CallbackInfo info){
+        renderStack.set(stack);
+    }
+    
+    @ModifyArg(method = "method_4015", at = @At(
+        value = "INVOKE",
+        target = "Lnet/minecraft/client/render/item/ItemRenderer;renderModelWithTint(Lnet/minecraft/client/render/model/BakedModel;I)V"
+    ), index = 1)
+    public int itemColor(int originalColor){
+        ItemStack stack = renderStack.get();
+        int color = originalColor; //0xFF8040CC;
+        if(stack != null){
+            CompoundTag tag = stack.getTag();
+            if(tag != null && tag.containsKey("gud")){
+                CompoundTag gudTag = tag.getCompound("gud");
+                if(gudTag.containsKey("glitter")){
+                    color = gudTag.getInt("glitter") | 0xFF000000;
                 }
             }
-
-            GlStateManager.popMatrix();
         }
+        return color;
     }
 }
